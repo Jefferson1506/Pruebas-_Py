20892b6b0de422ab4cb5f77fa925cc7fe8941713
Jefferson1506 <jeffersondpalacio@unicesar.edu.co>
2023-05-04 11:11:33 -0500

    Fase 3: Refactorización de la prueba : Mensaje de hola
    La prueba fue exitosa , el método funciona de manera correcta y de una manera mas  general

--- test_PP.py
+++ test_PP.py
@@ -4,10 +4,22 @@
 # assert mesnajeHola();
 
 #Fase 2 : Escribir una prueba que pase de obligatoria : mensaje de hola
+# def test_mensaje():
+#     return"Hello FastAPI"
+
+#Fase 3 :Refactorización de la prueba : Mensaje de hola
 def test_mensaje():
-    return"Hello FastAPI"
+ a=mensajeHola(1)
+ print(a)
+ return a
 
-
+def mensajeHola(X):
+    if X == 1 :
+      mensaje = "Hello FastAPI"
+    
+    if X !=1 and X>0: 
+      mensaje = "HOLIS PAPU LINDO"
+      return mensaje
 
 
 
