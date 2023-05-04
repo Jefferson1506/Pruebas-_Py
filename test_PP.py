c47b6489390a2d15789b9385cbe06ec55bb12a9e
Jefferson1506 <jeffersondpalacio@unicesar.edu.co>
2023-05-04 11:08:47 -0500

    Fase 2 : Escribir una prueba que pase de obligatoria : mensaje de hola
    La prueba fue correcta , en este caso de manera obvia se retorno la respuesta esperada

--- test_PP.py
+++ test_PP.py
@@ -1,4 +1,17 @@
 
-#Fase 1 : Escribir una prueba que falle : mesaje de hola
+#Fase 1 : Escribir una prueba que falle : mensaje de hola
+# def test_mensaje():
+# assert mesnajeHola();
+
+#Fase 2 : Escribir una prueba que pase de obligatoria : mensaje de hola
 def test_mensaje():
-    assert mesnajeHola();
+    return"Hello FastAPI"
+
+
+
+
+
+
+
+
+
